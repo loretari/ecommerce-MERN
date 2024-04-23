dc2ae77b1a0be77a1b6aa7c1697bf62f438d6883
loretari <loretari@gmail.com>
2024-04-23 09:28:04 +0300

    init: change CORS configuration in server side

--- server/index.js
+++ server/index.js
@@ -26,6 +26,14 @@ mongoose
         })
     })
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://eclectic-duckanoo-a18e26.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 
 app.use(cors({
     origin: 'https://eclectic-duckanoo-a18e26.netlify.app',
