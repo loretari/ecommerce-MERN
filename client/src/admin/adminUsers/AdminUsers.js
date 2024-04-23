ce417f63317e955cddcbf37ff5064834683df0fd
loretari <loretari@gmail.com>
2024-04-23 08:21:24 +0300

    init: initial first commit and add client directory

---
+++ client/src/admin/adminUsers/AdminUsers.js
@@ -0,0 +1,129 @@
+import React, {useEffect} from "react";
+import "./adminUsers.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import {useDispatch, useSelector} from "react-redux";
+import axios from "axios";
+import {deleteClientFailure, deleteClientSuccess, getClientFailure, getClientSuccess} from "../../redux/clientSlice";
+
+const AdminUsers =() => {
+
+const users = useSelector((state) => state.client.clients);
+
+const dispatch = useDispatch();
+
+useEffect(() => {
+    const getUsers = async () => {
+        try {
+            const res = await axios.get(`https://mern-ecommerce-app-clqa.onrender.com/users/`)
+            if (res.status === 200) {
+                const data = res.data;
+                dispatch(getClientSuccess(data))
+            } else {
+                console.error("Failed to fetch clients")
+            }
+
+        } catch (error) {
+            console.error('Axios error:', error)
+            dispatch(getClientFailure(error));
+        }
+    }
+    getUsers();
+}, [dispatch])
+
+    const handleDelete = async (id) => {
+    try {
+        const res = await axios.delete(`https://mern-ecommerce-app-clqa.onrender.com/users/${id}`)
+        if (res.status === 200) {
+            console.log("Product successfully deleted")
+            dispatch (deleteClientSuccess(id));
+        } else{
+            console.error("Failed to delete user:", res.statusText);
+            dispatch(deleteClientFailure("Failed to delete product"))
+        }
+    } catch (error) {
+        console.error("Failed to delete client:", error.message);
+        dispatch(deleteClientFailure(error));
+    }
+    }
+
+    const columns = [
+        {
+            field: "_id",
+            headerName: "ID",
+            width: 100 },
+
+        {
+            field: "username",
+            headerName: "Username",
+            width: 200,
+            renderCell: (params) => {
+                return (
+                    <div className="productListItem">
+                        <img className="productListImg" src={params.row.avatar} alt="" />
+                        {params.row.username}
+                    </div>
+                );
+            },
+        },
+
+        {
+            field: "email",
+            headerName: "Mail",
+            width: 200,
+            editable: true,
+        },
+
+        {
+            field: "isAdmin",
+            headerName: "Admin",
+            width: 150,
+        },
+
+        {
+            field: "action",
+            headerName: "Action",
+            width: 200,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <Link to={"/admin/user/" + params.row._id}>
+                            <button className="productListEdit">Edit</button>
+                        </Link>
+                        <DeleteOutlineOutlinedIcon
+                            className="productListDelete"
+                            onClick={() => handleDelete(params.row._id)}
+
+                        />
+
+                    </>
+                );
+            },
+        },
+    ];
+
+
+return (
+    <div className= "user">
+        <Link to="/admin/home" className="userAddButton">
+            Back
+        </Link>
+        <div style={{ height: 600, width: 1100, marginTop:30, display:"flex",justifyContent:"center"}}>
+            <Link to="/newUser">
+                <button className="newProductButton">Create New User </button>
+            </Link>
+            <DataGrid
+                rows={users}
+                columns={columns}
+                getRowId={(row) => row._id}
+                pageSize={8}
+                rowsPerPageOptions={[8]}
+                disableSelectionOnClick
+            />
+        </div>
+    </div>
+    )
+}
+
+export default AdminUsers;
---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import "./login.css";
+import {Link, useNavigate} from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {loginSuccess} from "../../redux/userSlice";
+import OAuth from "../../components/OAuth/OAuth";
+
+const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            const res = await fetch(`https://mern-ecommerce-app-clqa.onrender.com/auth/login`, {
+
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({username, email, password})
+            })
+            if (res.ok) {
+                const data = await res.json();
+                console.log("user", data)
+                dispatch(loginSuccess (data));
+                navigate("/")
+            } else {
+                const errorData = await res.json();
+                if (errorData && errorData.error === "Wrong password") {
+                    setError(true);
+                } else {
+                    setError(false)
+                }
+            }
+
+        } catch (error) {
+            setError(true);
+            console.log("Login failed:", error);
+            setTimeout(() => {
+                setError(false)
+            }, 3000)
+        }
+
+    }
+
+
+
+    return (
+        <div id= "login" className= "login-container">
+            <div className= "login-wrapper">
+                <h1 className= "login-tittle">LOG IN</h1>
+                <form onSubmit= {handleLogin} className= "login-form">
+                    <input  type= "username" className= "login-input" placeholder="Username"
+                            onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input  type= "email" className= "login-input" placeholder="Email"
+                            onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <input type= "password" className= "login-input" placeholder="Password"
+                           onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button className= "login-button" >LOGIN</button>
+                    <OAuth/>
+                    {error &&  (
+                        <div className= "login-error">
+                            Wrong username or password! Please try again.
+                        </div>
+                    )}
+                    <p>Don't have an account?
+                        <Link className= "login-link"
+                              to= "/register"
+                        >  CREATE A NEW ACCOUNT</Link>
+                    </p>
+                </form>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Login;
